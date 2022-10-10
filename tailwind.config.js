44f656589ca635f09ba13883174924597ef96fdf
Kachi <astrokachi@gmail.com>
2022-10-10 15:27:25 +0100

    added custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,35 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1D2123",
+        alt: "#1A1E1F",
+        trans: "rgba(255, 255, 255, 0.25)",
+        light: "#EFEEE0",
+        blueish: "#609EAF"
+      }, 
+
+      fontFamily: {
+        quick: 'Quicksand, sans-serif'
+      },
+
+      animation: {
+        sliding: "slide 1s forwards ",
+        closing: "close 2s forwards "
+      },
+
+      keyframes: {
+        slide : {
+          '0%': {transform: 'translateX(400px) '},
+          '100%': {transform: "translateX(0px) "}
+        },
+        close: {
+          '0%': {transform: "translateX(0px)"},
+          '100%': {transform: "translateX(800px)"},
+        }
+      }
+    },
   },
   plugins: [],
 }
